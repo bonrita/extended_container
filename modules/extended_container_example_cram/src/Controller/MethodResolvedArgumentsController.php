02a898ee592769654dea3e8ff9fb568a7cab3f03
Bonaventure Wani <bonaventure.wani@oneshoe.nl>
2020-02-03 11:04:51 +0100

    Examples: Add controller methods example.

--- modules/extended_container_example_cram/src/Controller/MethodResolvedArgumentsController.php
+++ modules/extended_container_example_cram/src/Controller/MethodResolvedArgumentsController.php
@@ -4,6 +4,39 @@
 namespace Drupal\extended_container_example_cram\Controller;
 
 
-class MethodResolvedArgumentsController {
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Theme\ThemeManagerInterface;
+use Drupal\language\ConfigurableLanguageManagerInterface;
+
+/**
+ * Example of a controller method whose dependencies are automatically resolved.
+ *
+ * @package Drupal\extended_container_example_cram\Controller
+ */
+class MethodResolvedArgumentsController extends ControllerBase {
+
+  /**
+   * The page.
+   *
+   * @param $step
+   * @param \Drupal\Core\Theme\ThemeManagerInterface $themeManager
+   * @param \Drupal\language\ConfigurableLanguageManagerInterface $languageManager
+   *
+   * @return array
+   */
+  public function page(
+    $step,
+    ThemeManagerInterface $themeManager,
+    ConfigurableLanguageManagerInterface $languageManager
+  ) {
+    $build['content'] = [
+      '#markup' => 'Current theme name: ' . $themeManager->getActiveTheme()
+          ->getName(
+          ) . ', current language site: ' . $languageManager->getCurrentLanguage(
+        )->getName() . ', Step: ' . $step,
+    ];
+
+    return $build;
+  }
 
 }
